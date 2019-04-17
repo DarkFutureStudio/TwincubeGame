f50fc267b4dd17d00c1468c506efc875a7d46848
etzl <erfanzamani3445@gmail.com>
2019-04-17 21:30:09 +0430

    new playerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,26 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float m_PlayerSpeed = 10;
-    public float m_ForceJump = 700;
+    public float m_JumpForce = 700;
+    public float m_MoveSpeed = 10;
 
-    GameObject[] m_players;
+    Rigidbody2D[] m_player = new Rigidbody2D[2];
     bool m_isJump = false;
+    float m_horizontalInput = 0;
 
     private void Awake()
     {
         for (int i = 0; i < transform.childCount; i++)
         {
-            m_players[i] = transform.GetChild(i).gameObject;
+            m_player[i] = transform.GetChild(i).GetComponent<Rigidbody2D>();
         }
     }
     private void Update()
     {
         if (Input.GetButtonDown("Jump"))
             m_isJump = true;
+        m_horizontalInput = Input.GetAxis("Horizontal");
     }
     private void FixedUpdate()
     {
@@ -29,8 +29,13 @@ public class PlayerController : MonoBehaviour
             Jumping();
             m_isJump = false;
         }
+
+        transform.Translate(new Vector2(m_horizontalInput * m_MoveSpeed * Time.fixedDeltaTime, 0));
     }
-    private void Jumping()
+    
+    void Jumping()
     {
+        m_player[0].AddForce(new Vector2(0, m_JumpForce));
+        m_player[1].AddForce(new Vector2(0, -m_JumpForce));
     }
 }
