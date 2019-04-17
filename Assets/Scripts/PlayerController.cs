ba4565eca513bf5f7eed9d5245c192ca9acbc317
etzl <erfanzamani3445@gmail.com>
2019-04-17 20:56:02 +0430

    changing playerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,21 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float m_PlayerSpeed = 10;
     public float m_ForceJump = 700;
 
-    protected Rigidbody2D m_playerRigid;
+    GameObject[] m_players;
     bool m_isJump = false;
 
     private void Awake()
     {
-        m_playerRigid = GetComponent<Rigidbody2D>();
-    }
-    void Start()
-    {
-        m_playerRigid.velocity = new Vector2(m_PlayerSpeed, m_playerRigid.velocity.y);
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            m_players[i] = transform.GetChild(i).gameObject;
+        }
     }
     private void Update()
     {
