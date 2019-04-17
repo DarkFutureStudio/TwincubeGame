c5b7a9a9f83e22859fdf87880a0732dd3267218d
etzl <erfanzamani3445@gmail.com>
2019-04-17 22:59:33 +0430

    win completed

--- Assets/Scripts/TriggerWin.cs
+++ Assets/Scripts/TriggerWin.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class TriggerWin : MonoBehaviour
 {
+    public GameObject m_WinCanv;
 
+    static int m_counter = 0;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Finish"))
+            m_counter++;
+    }
+    private void LateUpdate()
+    {
+        if (m_counter == 2)
+            m_WinCanv.SetActive(true);
+    }
 }
