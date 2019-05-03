03d130ad3e94cd17fbacb94b239dc5338faf7ec1
sinnerman0 <hoseinbs16@gmail.com>
2019-05-03 13:03:58 +0430

    prob

---
+++ Assets/Levelchanger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Levelchanger : MonoBehaviour
+{
+    public Animator animator;
+
+    private int levelToLoad;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(1);
+        }
+    }
+    public void FadeToLevel (int levelIndex)
+    {
+        levelToLoad = levelIndex; 
+        animator.SetTrigger("Fadeout");
+    }
+
+    public void Onfadecomplete ()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+}
