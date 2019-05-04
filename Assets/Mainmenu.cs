f9fb853aec90656e9dab045331c0811eae489698
sinnerman0 <hoseinbs16@gmail.com>
2019-05-04 20:23:22 +0430

    big change in UI sys

---
+++ Assets/Mainmenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Mainmenu : MonoBehaviour
+{
+    public string LevelToLoad = "Mainlevel";
+
+    public SceneFader sceneFader;
+    public void Play ()
+    {
+        sceneFader.FadeTo(LevelToLoad);
+    }
+    public void Quit ()
+    {
+        Debug.Log("Exiting");
+        Application.Quit();
+    }
+}
---
+++ Assets/buttonload.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class buttonload : MonoBehaviour
+{
+    public string LevelToLoad = "";
+    public string LevelToLoad1 = "";
+
+    public SceneFader sceneFader;
+    public void b1()
+    {
+        sceneFader.FadeTo(LevelToLoad);
+    }
+    public void b2()
+    {
+        sceneFader.FadeTo(LevelToLoad1);
+    }
+}
