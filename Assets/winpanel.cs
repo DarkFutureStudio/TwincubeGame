44c8852f58cee220cb3e16a15db463a90c03da4b
sinnerman0 <hoseinbs16@gmail.com>
2019-05-18 19:56:31 +0430

    win panel complete

---
+++ Assets/winpanel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class winpanel : MonoBehaviour
+{
+    public Button[] levelselect;
+    public SceneFader fader;
+
+    public void Select(string levelname)
+    {
+        fader.FadeTo(levelname);
+    }
+  
+    public void retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
