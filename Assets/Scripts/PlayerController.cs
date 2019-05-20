733d0222e0ff6c90101da72efa8705adcace3004
etzl <erfanzamani3445@gmail.com>
2019-05-20 11:59:37 +0430

    some new changes in scripts "PlayerController"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,40 +1,37 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float jumpForce = 700;
-    public float moveSpeed = 10;
-    public int jumpLimit = 6;
+    public float jumpForce = 170f;
+    public float moveSpeed = 10f;
     public Joystick joystick;
 
-    Rigidbody2D[] m_Player = new Rigidbody2D[2];
-    float m_HorizontalInput = 0;
-    Restart m_GameReset = new Restart();
+    Rigidbody2D m_Rigidbody;
+    float m_HorizontalAxis;
 
-    void Awake()
+    void Start()
     {
-        //get players rigidbody in children...
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            m_Player[i] = transform.GetChild(i).GetComponent<Rigidbody2D>();
-        }
-    }    
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
     void FixedUpdate()
     {
-        m_HorizontalInput = joystick.Horizontal;
-        //move the players...
-        transform.Translate(new Vector2(m_HorizontalInput * moveSpeed * Time.fixedDeltaTime, 0));
+        m_HorizontalAxis = joystick.Horizontal;
+        Vector2 movement = new Vector2(m_HorizontalAxis * moveSpeed, 0);
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement * Time.deltaTime);
     }
-    
-    public void Jumping()
-    {
-        m_Player[0].AddForce(new Vector2(0, jumpForce));
-        m_Player[1].AddForce(new Vector2(0, -jumpForce));
 
-        jumpLimit--;
-        if (jumpLimit == 0)
-            m_GameReset.RestartScene();
+    public void Jump(int index)
+    {
+        Vector2 jumpDirection;
+
+        if (index == 0)
+            jumpDirection = new Vector2(0f, jumpForce);
+        else
+            jumpDirection = new Vector2(0f, -jumpForce);
+
+        m_Rigidbody.AddForce(jumpDirection);
     }
 }
