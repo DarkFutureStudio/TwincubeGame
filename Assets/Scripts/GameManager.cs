55b234da65fa972afad6573f65380c86dbfbb417
etzl <erfanzamani3445@gmail.com>
2019-05-24 16:56:52 +0430

    win added to game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,10 @@ public class GameManager : MonoBehaviour
     public float playerMoveSpeed = 5f;
     public float playerJumpForce = 170f;
     public int jumpLimit;
+    public bool isWin;
 
     int m_CurrentSceneIndex;
+    int m_TargetHoles;
 
     private void Start()
     {
@@ -19,12 +21,30 @@ public class GameManager : MonoBehaviour
     {
         if (jumpLimit < 0)
         {
-            SceneManager.LoadScene(m_CurrentSceneIndex);
+            //if you lose
+            LoadLevel(m_CurrentSceneIndex);
         }
+        if (m_TargetHoles == 2)
+        {
+            //if you won
+            if (isWin)
+            {
+                LoadLevel(m_CurrentSceneIndex + 1);
+            }
+        }
+    }
+
+    void LoadLevel(int level)
+    {
+        SceneManager.LoadScene(level);
     }
 
     public void ButtonPressed()
     {
         jumpLimit--;
+    }
+    public void IncreamentTarget()
+    {
+        m_TargetHoles++;
     }
 }
