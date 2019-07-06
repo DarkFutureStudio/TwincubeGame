1f13c56a82be01b66df63d5f150c2eb2afe1f740
etzl <erfanzamani3445@gmail.com>
2019-07-06 14:15:39 +0430

    making jumper cube in progress

--- Assets/Scripts/Non-player Controllers/JumperCube.cs
+++ Assets/Scripts/Non-player Controllers/JumperCube.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class JumperCube : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform reachPoint;
+    public float speed;
 
-    // Update is called once per frame
-    void Update()
+    Rigidbody2D m_Rigidbody;
+    Vector2 m_FirstPosition;
+    Vector2 m_Target;
+
+    private void Start()
     {
-        
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+
+        m_FirstPosition = transform.position;
+    }
+    private void FixedUpdate()
+    {
+        Vector2 target = Vector2.MoveTowards
+            (transform.position, m_Target, speed * Time.deltaTime);
+
+        m_Rigidbody.MovePosition(target);
+
+        if (transform.position == reachPoint.position)
+        {
+            m_Target = m_FirstPosition;
+        }
+        else if (transform.position == (Vector3)m_FirstPosition)
+        {
+            m_Target = reachPoint.position;
+        }
     }
 }
